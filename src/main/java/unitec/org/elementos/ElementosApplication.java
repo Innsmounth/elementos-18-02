063e76fe7b0a7cef5580665db9b6045094c11b91
T-107 <T-107@PC197>
2018-01-30 21:39:06 -0600

    Retana Ladino Juan Felipe

--- src/main/java/unitec/org/elementos/ElementosApplication.java
+++ src/main/java/unitec/org/elementos/ElementosApplication.java
@@ -1,5 +1,6 @@
 package unitec.org.elementos;
 
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,6 +17,24 @@ public class ElementosApplication implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         Mensajitos n = new Mensajitos ("primero","holaa");
-        repoMensa.save(n);
+        //repoMensa.save(n);
+        
+        //Probaremos el de buscbar todos
+        ArrayList<Mensajitos>mensajitos=(ArrayList<Mensajitos>)repoMensa.findAll();
+        for(Mensajitos mensa: mensajitos)
+        {
+        System.out.println(mensa.getTitulo()); 
+        }
+        
+        //Ahora probaremos en buscar por ID
+       Mensajitos m1= repoMensa.findOne(1);
+        System.out.println(m1.getTitulo()); 
+        
+        //Update actualizacion
+        
+        repoMensa.save(new Mensajitos(1,"otro","malo"));
+        
+        //borrar
+        repoMensa.delete(1);
     }
 }
